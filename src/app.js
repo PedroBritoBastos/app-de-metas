fbc6cf9088adc976a10984b0005459c72038f5a2
pedro <pedrobritobastos234@gmail.com>
2025-05-10 18:09:44 -0300

    rota configurada

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const noteRoutes = require('./routes/noteRoutes');
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({
+      extended: true
+    }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', noteRoutes);
+  }
+}
+
+module.exports = new App().app;
